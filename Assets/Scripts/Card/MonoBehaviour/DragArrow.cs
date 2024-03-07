21ab857d6cb0408c1aedba567f75f96d4abc3c98
Huaiyuan Jing <ls.hyjing@gmail.com>
2024-03-07 11:45:49 -0600

    Add arc modifier and point count to ArrowLine prefab

--- Assets/Scripts/Card/MonoBehaviour/DragArrow.cs
+++ Assets/Scripts/Card/MonoBehaviour/DragArrow.cs
@@ -15,7 +15,39 @@ public class DragArrow : MonoBehaviour
             Input.mousePosition.y, 
             10
         ));
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, mousePos);
+        SetArrowPosition();
+    }
+    public void SetArrowPosition()
+    {
+        Vector3 cardPosition = transform.position; // 卡牌位置
+        Vector3 direction = mousePos - cardPosition; // 从卡牌指向鼠标的方向
+        Vector3 normalizedDirection = direction.normalized; // 归一化方向
+
+        // 计算垂直于卡牌到鼠标方向的向量
+        Vector3 perpendicular = new(-normalizedDirection.y, normalizedDirection.x, normalizedDirection.z);
+
+        // 设置控制点的偏移量
+        Vector3 offset = perpendicular * arcModifier; // 你可以调整这个值来改变曲线的形状
+
+        Vector3 controlPoint = (cardPosition + mousePos) / 2 + offset; // 控制点
+
+
+        lineRenderer.positionCount = pointsCount; // 设置 LineRenderer 的点的数量
+
+        for (int i = 0; i < pointsCount; i++)
+        {
+            float t = i / (float)(pointsCount - 1);
+            Vector3 point = CalculateBezierPoint(t, cardPosition, controlPoint, mousePos);
+            lineRenderer.SetPosition(i, point);
+        }
+    }
+    Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2) {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        Vector3 p = uu * p0;
+        p += 2 * u * t * p1;
+        p += tt * p2;
+        return p;
     }
 }
