90ae1cc722630101d346af66d61b150269535b2d
Huaiyuan Jing <ls.hyjing@gmail.com>
2024-03-06 22:42:42 -0600

    Add curve layout support to CardLayoutManager

--- Assets/Scripts/Manager/CardLayoutManager.cs
+++ Assets/Scripts/Manager/CardLayoutManager.cs
@@ -6,9 +6,16 @@ public class CardLayoutManager : MonoBehaviour
     public bool isHorizontal;
     public float maxWidth = 7f;
     public float cardSpacing = 2f;
+    [Header("Curve Layout")]
+    public float angleBetweenCards = 7f;
+    public float radius = 17f;
     public Vector3 centerPoint;
     [SerializeField] private List<Vector3> cardPositions = new();
     private List<Quaternion> cardRotations = new();
+    private void Awake()
+    {
+        centerPoint = isHorizontal ? Vector3.up * -4.5f : Vector3.up * -21.5f;
+    }
     public CardTransform GetCardTransform(int index, int totalCards)
     {
         CalculatePosition(totalCards, isHorizontal);
@@ -20,14 +27,38 @@ public class CardLayoutManager : MonoBehaviour
         cardRotations.Clear();
         if (horizontal)
         {
-            float currentWidth = cardSpacing * (numberOfCards - 1);
-            float totalWidth = Mathf.Min(maxWidth, currentWidth);
-            float currentSpacing = totalWidth > 0 ? totalWidth / (numberOfCards - 1) : 0;
-            for (int i = 0; i < numberOfCards; i++)
-            {
-                cardPositions.Add(new Vector3(-totalWidth / 2 + i * currentSpacing, centerPoint.y, 0));
-                cardRotations.Add(Quaternion.identity);
-            }
+            Calculate_Horizontal_Layout(numberOfCards);
+        }
+        else
+        {
+            Calculate_Curve_Layout(numberOfCards);
+        }
+    }
+    private void Calculate_Curve_Layout(int numberOfCards)
+    {
+        float cardAngle = (numberOfCards - 1) * angleBetweenCards / 2;
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            var pos = FanCardPosition(cardAngle - i * angleBetweenCards);
+            var rotation = Quaternion.Euler(0, 0, cardAngle - i * angleBetweenCards);
+        }
+    }
+    private Vector3 FanCardPosition(float angle)
+    {
+        return new Vector3(
+            centerPoint.x - radius * Mathf.Sin(angle * Mathf.Deg2Rad), centerPoint.y + radius * Mathf.Cos(angle * Mathf.Deg2Rad),
+            0
+        );
+    }
+    private void Calculate_Horizontal_Layout(int numberOfCards)
+    {
+        float currentWidth = cardSpacing * (numberOfCards - 1);
+        float totalWidth = Mathf.Min(maxWidth, currentWidth);
+        float currentSpacing = totalWidth > 0 ? totalWidth / (numberOfCards - 1) : 0;
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            cardPositions.Add(new Vector3(-totalWidth / 2 + i * currentSpacing, centerPoint.y, 0));
+            cardRotations.Add(Quaternion.identity);
         }
     }
 }
